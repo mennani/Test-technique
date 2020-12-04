823b0ab9dd20252848e33238dad1e5b04fcf23ae
Hamza Mennani <37930534+mennani@users.noreply.github.com>
2020-12-04 17:48:36 +0100

    Update views.py

--- API/views.py
+++ API/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
+"""
+Cette page contient deux fonctions, une fonction de vue my_view(request) est la fonction API_controller(request) utilisée pour simplifier la programmation
+
+La fonction API_controller prend un seul paramètre qu'est la requête envoyée par le client, et elle renvoie un dictionnaire dont 
+les clés sont des chaines de caracètre représentant les noms des champs du formulaire (paramètres + résultat), 
+et les valeurs du dictionnaire sont les valeurs des champs du formulaire
+ 
+La fonction my_view prend un seul paramètre qu'est la requête envoyée par le client,
+puis, la fonction teste si la requête est du type POST ou non, c'est si le cas, elle fait appel à la fonction API_controller pour récupérer le résultat
+sinon, elle crée un dictionnaire contenant les résultats obtenus en utilisant les paramètres par défaut.
+Ensuite, la fonction envoie le dictionnaire au client (index.html) par l'intermédiaire de la fonction render
+
+"""
+
 @csrf_exempt
-def index(request):
+def my_view(request):
 
     if request.method == 'POST':
-        reponse = API(request)
+        reponse = API_controller(request)
+
     else:
         reponse ={
             "multiple" : 7,
@@ -20,14 +35,8 @@ def index(request):
 
 
 
-def API(request):
-    """
+def API_controller(request):
 
-    :param Multiple:
-    :param seuil_max:
-    :param frequence:
-    :return:
-    """
     if "Multiple" in request.POST.keys():
         multiple = int(request.POST["Multiple"])
     else:
